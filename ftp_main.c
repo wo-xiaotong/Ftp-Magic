86487e457ce29bd765201c3afbc9aeb58408fc09
jarvis4tonny <1096232062@qq.com>
2016-04-28 09:24:53 +0800

    test fetch

--- ftp_main.c
+++ ftp_main.c
@@ -1,10 +1,12 @@
 #include<stdio.h>
-#include"connect_func.h"
+
+extern int init_socket(const char* ,const int);
+extern int close_socket(int);
+
 
 int main()
 {
 	int fd=init_socket("127.0.0.1",21);
-	printf("%d\n",fd);
 	close_socket(fd);
 	return 0;
 }
